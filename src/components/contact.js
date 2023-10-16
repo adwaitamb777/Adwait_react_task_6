bae5d8ca89166e2c91dacd69c20bac8f5d21f2dc
adwaitamb777 <144168144+adwaitamb777@users.noreply.github.com>
2023-10-16 19:51:41 +0530

    Add files via upload

---
+++ src/components/contact.js
@@ -0,0 +1,15 @@
+
+
+import React from 'react';
+
+function Contact() {
+  return (
+    <section className="contact">
+      <h2>Contact Me</h2>
+      <p>Email: adwaitambekar000@gmail.com</p>
+      <p>Phone: 9404025777</p>
+    </section>
+  );
+}
+
+export default Contact;
